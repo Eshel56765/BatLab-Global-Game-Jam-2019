5a8b7baeac3fbac677101550b34a555234eb91aa
Eshel56765 <Eshel56765@gmail.com>
2019-01-24 21:21:27 +0200

    Imported textmeshpro assets, added minigame base class, and maybe fixed gitignore

---
+++ Assets/Scripts/Minigames/Minigame.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Minigame : MonoBehaviour
+{
+    public GameObject UI;
+
+    // Start is called before the first frame update
+    void StartGame()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    protected abstract void EndGame();
+}
