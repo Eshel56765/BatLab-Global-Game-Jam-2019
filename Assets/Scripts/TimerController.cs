5522af25c41004fa3ac902785a8949d27e346e6a
Sagi Dadon <sagi.dddd@gmasil.com>
2019-01-25 01:14:11 +0200

    Added the Timer to the scene

---
+++ Assets/Scripts/TimerController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimerController : MonoBehaviour
+{
+    public Image Timer;
+    public float StartingTime;
+    public float Minutes, Seconds;
+    public Text MinutesText, SecondsText;
+    
+    private float timeElapsed, totalTime;
+
+    void Start()
+    {
+        StartingTime *= 60;
+        totalTime = StartingTime;
+        Timer.fillAmount = 1;
+        StartCoroutine(ClocksTicking());
+    }
+
+    IEnumerator ClocksTicking()
+    {
+        do
+        {
+            timeElapsed += Time.deltaTime;
+            Timer.fillAmount = Mathf.Clamp01((totalTime - timeElapsed) / totalTime);
+            Minutes = Mathf.Clamp(Mathf.FloorToInt((totalTime - timeElapsed) / 60f),0,Mathf.Infinity);
+            Seconds = Mathf.Clamp(Mathf.FloorToInt((totalTime - timeElapsed) % 60f), 0, Mathf.Infinity);
+
+            MinutesText.text = Minutes.ToString();
+            SecondsText.text = Seconds.ToString();
+
+            if(Minutes < 10)
+               MinutesText.text = "0" + Minutes.ToString();
+            if (Seconds < 10)
+                SecondsText.text = "0" + Seconds.ToString();
+                
+            yield return null;
+        }
+        while (Timer.fillAmount > 0);
+    }
+}
