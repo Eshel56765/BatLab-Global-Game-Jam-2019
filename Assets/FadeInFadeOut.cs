1b49abd9bcce9d694049b83d175d7b14220d78d2
Sagi Dadon <sagi.dddd@gmasil.com>
2019-01-25 16:27:16 +0200

    Added the fadeInFadeOut And combined the ui to work

---
+++ Assets/FadeInFadeOut.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class FadeInFadeOut : MonoBehaviour
+{
+    public bool CanGoToWork;
+    public bool CanFade;
+    public Collider PlayerBodyCol;
+
+    public CashManager cashManager;
+
+    public Image BlackScreen;
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.F))
+        {
+            StartCoroutine(FadeOut());
+        }
+    }
+
+    public void OnTriggerEnter(Collider collider)
+    {
+        if(collider.Equals(PlayerBodyCol))
+        {
+            CanGoToWork = true;
+        }
+    }
+
+    public void OnTriggerExit(Collider collider)
+    {
+        if (collider.Equals(PlayerBodyCol))
+        {
+            CanGoToWork = false;
+        }
+    }
+
+    IEnumerator FadeIn()
+    {
+        for (float i = 1; i >= 0; i -= Time.deltaTime)
+        {
+            BlackScreen.fillAmount = Mathf.Clamp01(i);
+            yield return null;
+        }
+        BlackScreen.fillAmount = 0;
+        CanFade = true;
+    }
+
+    IEnumerator FadeOut()
+    {
+        if (CanGoToWork && CanFade)
+        {
+            for (float i = 0; i <= 1; i += Time.deltaTime)
+            {
+                BlackScreen.fillAmount = Mathf.Clamp01(i);
+                CanFade = false;
+                yield return null;
+            }
+            BlackScreen.fillAmount = 1;
+            StartCoroutine(FadeIn());
+        }
+    }
+
+}
