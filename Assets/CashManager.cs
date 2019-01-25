ea889c3728fb8e27063d178f1ccd93d643aba405
Sagi Dadon <sagi.dddd@gmasil.com>
2019-01-25 13:08:01 +0200

    Added the Money shits call func "AddMoney" to add the wanted amount

---
+++ Assets/CashManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CashManager : MonoBehaviour
+{
+    public Text Cash;
+    public ParticleSystem MoneysExplosion;
+
+    int maxAmountOfMoneys = 99999;
+
+    public void AddMoney(int Amount)
+    {
+        int i = int.Parse(Cash.text);
+
+        if (i + Amount > maxAmountOfMoneys)
+            Cash.text = maxAmountOfMoneys.ToString();
+        else
+            Cash.text = (i + Amount).ToString().PadLeft(maxAmountOfMoneys.ToString().Length, '0');
+
+        MoneysExplosion.Play();
+    }
+}
