2643a98f0a3b9ffc8aca8c0504351ab64d45d278
venuspony <ayay666@gmail.com>
2019-01-25 12:40:09 +0200

    added player movement(fix 56)

---
+++ Assets/Scripts/MovementController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementController : MonoBehaviour
+{
+    public Camera PlayerCamera;
+    public float Speed;
+    private Rigidbody rigidBody;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 velocity = new Vector3();
+        Vector3 cameraForward = PlayerCamera.transform.forward;
+        cameraForward.y = 0;
+        cameraForward.Normalize();
+        Vector3 cameraRight = PlayerCamera.transform.right;
+        cameraRight.y = 0;
+        cameraRight.Normalize();
+        velocity = Input.GetAxis("Vertical") * cameraForward ;
+        velocity += Input.GetAxis("Horizontal") * cameraRight ;
+        Debug.Log(velocity);
+        transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.LookRotation(velocity), 720 * Time.deltaTime);
+        rigidBody.velocity = velocity * Speed;
+    }
+}
