a98b1420aa1bd1c19ad17ea5fdd34a738b186f26
Sagi Dadon <sagi.dddd@gmasil.com>
2019-01-26 02:43:31 +0200

    fix

---
+++ Assets/RandomColor.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RandomColor : MonoBehaviour
+{
+    public Graphic thang;
+
+    private void Update()
+    {
+        if (null != thang)
+            thang.color = Random.ColorHSV(0, 1, 0, 1, 0, 1, 1, 1);
+
+    }
+}
---
+++ Assets/Scripts/ProblemTrigger.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProblemTrigger : MonoBehaviour
+{
+    public GameObject MiniGamePrefab;
+    private Collider problemCollider;
+
+    private GameObject CurrentGame;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        problemCollider = gameObject.GetComponent<Collider>();
+    }
+
+    private void OnTriggerStay(Collider Col)
+    {
+        if (Col.CompareTag("Player") && Input.GetKeyDown(KeyCode.F) && CurrentGame == null)
+        {
+            CurrentGame = Instantiate(MiniGamePrefab, GameManger.Instance.UICanvas);
+        }
+    }
+}
